9575620ccba442f354e38fd1bfbbe695f2a772eb
Jeff <jf.gasp@gmail.com>
2021-11-03 22:43:11 +0100

    finish

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,74 +3,75 @@ import axios from 'axios';
 import Form from './components/Form';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { isEmpty } from './components/utils';
 import AlertResult from './components/AlertResult';
 import Member from './components/Member';
 import { API_URL } from './config/config';
 
+interface IMember {
+    _id: string;
+    name: string;
+}
+
 const App = () => {
-    const [members, setMembers] = useState<any[]>([]); // all data
+    const [members, setMembers] = useState<IMember[]>([]); // all data
     const [alertSuccess, setAlertSuccess] = useState(''); // alert success message
     const [alertError, setAlertError] = useState(''); // alert error message
     const [name, setName] = useState(''); // input content
+    const [timeoutID, setTimeoutID] = useState<NodeJS.Timeout | null>(null); // error timeout
 
-    // Fetch all members
+    // Get all members
     useEffect(() => {
-        const fetchMembers = async () => {
-            await axios
-                .get(`${API_URL}/api/member`)
-                .then((res) => {
-                    console.log(res.data);
-
-                    setMembers(res.data);
-                })
-                .catch((err) => console.log(err));
-        };
-        fetchMembers();
+        axios
+            .get(`${API_URL}/api/member`)
+            .then((res) => setMembers(res.data))
+            .catch((err) => console.log(err));
     }, []);
 
     // Submit member
     const handleSubmit = (e: HTMLFormElement) => {
         e.preventDefault();
-        const newMember = async () => {
-            await axios
-                .put(`${API_URL}/api/member`, { name })
-                .then((res) => {
-                    if (res.data.errors) {
-                        setAlertError(res.data.errors);
-                        setAlertSuccess('');
-                    } else {
-                        setMembers((old) => [...old, res.data.member]);
-                        setAlertSuccess(`${name} a été ajouté`);
-                        setAlertError('');
-                    }
+        axios
+            .put(`${API_URL}/api/member`, { name })
+            .then((res) => {
+                if (res.data.errors) {
+                    setAlertError(res.data.errors);
+                    setAlertSuccess('');
+                } else {
+                    setMembers((members) => [...members, res.data.member]);
+                    setAlertSuccess(`${name} a été ajouté au voyage !`);
+                    setAlertError('');
+                }
+
+                timeoutID && clearTimeout(timeoutID);
+
+                setTimeoutID(
                     setTimeout(() => {
                         setAlertError('');
                         setAlertSuccess('');
-                    }, 3000);
-                })
-                .catch((err) => console.log(err));
-        };
-        newMember();
+                    }, 3000)
+                );
+            })
+            .catch((err) => console.log(err));
+
         setName('');
     };
 
     // Delete member
     const handleDeleteMember = (id: string, name: string) => {
-        const deleteMember = async () => {
-            await axios.delete(`${API_URL}/api/member/${id}`, { data: { name } }).then((res) => {
-                if (res.data.message) {
-                    setAlertSuccess(res.data.message);
-                    setAlertError('');
-                    setMembers((old) => old.filter((m) => m._id !== id));
+        axios
+            .delete(`${API_URL}/api/member/${id}`, { data: { name } })
+            .then((res) => {
+                setAlertSuccess(`${res.data.name} a été supprimé du voyage !`);
+                setAlertError('');
+                setMembers((m) => m.filter((m) => m._id !== id));
 
-                    setTimeout(() => {
-                        setAlertSuccess('');
-                    }, 3000);
-                }
-            });
-        };
-        deleteMember();
+                timeoutID && clearTimeout(timeoutID);
+
+                setTimeout(() => {
+                    setAlertSuccess('');
+                }, 3000);
+            })
+            .catch((err) => console.log(err));
     };
     return (
         <>
--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,12 +1,5 @@
 export let API_URL = '';
 
-// const ON_PRODUCTION = true;
-
-// if (ON_PRODUCTION) {
-//     API_URL = '';
-// } else {
-//     API_URL = 'http://localhost:8000';
-// }
 if (process.env.NODE_ENV === 'production') {
     API_URL = '';
 } else if (process.env.NODE_ENV === 'development') {
