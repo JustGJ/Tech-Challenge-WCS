111b72b1ab8214910a9545066fd5fa812e7c9880
Jeff <jf.gasp@gmail.com>
2021-11-03 19:32:04 +0100

    backup

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,7 +1,14 @@
 export let API_URL = '';
 
-if (process.env.NODE_ENV === 'production') {
+const ON_PRODUCTION = true;
+
+if (ON_PRODUCTION) {
     API_URL = '';
-} else if (process.env.NODE_ENV === 'development') {
+} else {
     API_URL = 'http://localhost:8000';
 }
+// if (process.env.NODE_ENV === 'production') {
+//     API_URL = '';
+// } else if (process.env.NODE_ENV === 'development') {
+//     API_URL = 'http://localhost:8000';
+// }
